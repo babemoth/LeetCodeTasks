fdceab2a15ba8f5793018bc387f31ccf6c3e7106
kate <povisheva.k@yandex.ru>
2022-07-15 18:01:51 +0300

    Linked List Cycle done.

---
+++ src/EasyLevel/LinkedList/LinkedListCycle.java
@@ -0,0 +1,29 @@
+package EasyLevel.LinkedList;
+
+public class LinkedListCycle {
+    public static void main(String[] args) {
+
+    }
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast)
+                return true;
+        }
+
+        return false;
+    }
+}
